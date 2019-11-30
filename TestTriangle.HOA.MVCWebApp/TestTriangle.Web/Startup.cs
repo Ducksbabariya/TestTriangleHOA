04d6e9f52312d921c0a24c2faf05e7d7b175787e
Nitinkumar Vijay <Nitinkumar.Vijay@wolterskluwer.com>
2019-11-30 11:29:09 +0530

    css fixes

--- TestTriangle.HOA.MVCWebApp/TestTriangle.Web/Startup.cs
+++ TestTriangle.HOA.MVCWebApp/TestTriangle.Web/Startup.cs
@@ -57,15 +57,15 @@ namespace TestTriangleHOA.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
-            }
+            //}
 
             app.UseCookiePolicy();
             app.UseAuthentication();
